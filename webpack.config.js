4e6c3ecdda2739c3ae0a63898afd66a008ddd618
qsg1215 <1184053575@qq.com>
2017-12-26 00:01:36 +0800

    webpack配置公共参插件抽取

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,20 @@ var webpack = require("webpack");
 var htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
-        main: './main_android.js'
+        index: './main_android.js',
+        vendor: ['framework7', 'moment', 'axios', 'md5-hex','pinyin','echarts']
     },//入口文件
     output: {
-        // filename: 'bundle'+new Date().getTime()+'.js',
+        path: path.resolve(__dirname, './dist'),
+        filename: "[name].[chunkHash:8].js",
+        publicPath: './dist/',
+        chunkFilename: "[name].[chunkHash:8].js",
+       /* // filename: 'bundle'+new Date().getTime()+'.js',
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: './dist/',
         chunkFilename: "[chunkhash][name].js",
-        chunkLoadTimeout:120000,
+        chunkLoadTimeout:120000,*/
     },
     module: {
         loaders: [
@@ -31,7 +36,10 @@ module.exports = {
             inject:true,
             title:'webpack is good',
             chunks:['main']
-        })
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest'],
+        }),
     ],
   resolve: {// 现在你require文件的时候可以直接使用require('file')，不用使用require('file.coffee')
       extensions: ['.js', '.json', '.coffee']
